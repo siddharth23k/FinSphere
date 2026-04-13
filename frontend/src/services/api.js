b9b6fbd80495b1a890ee03be38f53b3e9ca77af6
siddharth23k <siddharthkumar0923@gmail.com>
2026-04-13 23:02:59 +0530

    fix: remove localhost api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+});
 
 // Attach token from localStorage automatically
 API.interceptors.request.use((config) => {
